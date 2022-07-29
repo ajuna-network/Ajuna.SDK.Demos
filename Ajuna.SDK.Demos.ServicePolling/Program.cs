8fd6c1a70f04701e833479776c5a7b576701e6ae
Dimitris Paxinos <dpaxinos@gmail.com>
2022-07-29 22:29:26 +0300

    Updated Service Polling Demo

--- Ajuna.SDK.Demos.ServicePolling/Program.cs
+++ Ajuna.SDK.Demos.ServicePolling/Program.cs
@@ -1,3 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.WebSockets;
+using Ajuna.NetApi.Model.Types.Primitive;
+using Ajuna.SDK.Demos.RestClient;
+using Ajuna.SDK.Demos.RestClient.Generated.Clients;
+using Ajuna.ServiceLayer.Model;
 
-Console.WriteLine("Hello, World!");
+namespace Ajuna.SDK.Demos.ServiceSubscription
+{
+    internal static class Program
+    {
+        // Websocket and API addresses of the Service layer - You need Ajuna.SDK.Demos.RestService running for this console app to run
+        private static String WebsocketUrl = "ws://localhost:61752/ws";
+        private static String ServiceUrl = "http://localhost:61752/";
+        
+        public static async Task Main(string[] args)
+        {
+            // Create BaseSubscriptionClient and connect
+            var subscriptionClient = new BaseSubscriptionClient(new ClientWebSocket());
+            await subscriptionClient.ConnectAsync(new Uri(WebsocketUrl), CancellationToken.None);
+             
+            // Create HttpClient
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(ServiceUrl)
+            };
+            
+            // Create SystemControllerClient
+            var systemControllerClient = new SystemControllerClient(httpClient, subscriptionClient);
+            
+            // Poll for Number Changes
+            var continuePolling = true;
+            
+            Console.WriteLine($"Starting Number Value Polling");
+
+            while (continuePolling)
+            {
+                 var newNumber = await systemControllerClient.GetNumber();
+                 Console.WriteLine($"Number is: {newNumber.Value}");
+                 Thread.Sleep(2000);
+                 
+                 if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Escape)
+                 {
+                     continuePolling = false;
+                 }
+            }
+            
+            Console.WriteLine($"Number Value Polling finished");
+        }
+
+        private static void HandleChange(StorageChangeMessage message)
+        {
+            Console.WriteLine("New Change: " + message.Data);
+        }
+    }
+}
