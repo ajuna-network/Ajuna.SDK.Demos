1479c0d05cbf399fa4afaf6244f972bd97b90b83
Dimitris Paxinos <dpaxinos@gmail.com>
2022-07-29 18:07:35 +0300

    Subscription Demos

---
+++ Ajuna.SDK.Demos.DirectSubscription/Program.cs
@@ -0,0 +1,67 @@
+﻿using Ajuna.NetApi;
+using Ajuna.NetApi.Model.Rpc;
+using Ajuna.SDK.Demos.NetApi.Generated;
+using Serilog;
+
+
+namespace Ajuna.SDK.Demos.DirectSubscription
+{
+    internal static class Program
+    {
+        private static string NodeUrl = "ws://127.0.0.1:9944";
+
+        public static async Task Main(string[] args)
+        {
+            // Instantiate the client
+           var client = new SubstrateClientExt(new Uri(NodeUrl));
+
+           // Display Client Connection Status before connecting
+           Console.WriteLine( $"Client Connection Status: {GetClientConnectionStatus(client)}");
+
+           await client.ConnectAsync();
+           
+           // Display Client Connection Status after connecting
+           Console.WriteLine( client.IsConnected ? "Client connected successfully" : "Failed to connect to node. Exiting...");
+
+           if (!client.IsConnected)
+               return;
+
+           // Subscribe to the changes of System.Number by registering a Callback for each Number Change  
+           await client.SubscribeStorageKeyAsync(Ajuna.SDK.Demos.NetApi.Generated.Model.FrameSystem.SystemStorage.NumberParams(),
+                   CallBackNumberChange, new CancellationTokenSource().Token);
+
+           Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Called on any number change.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription identifier.</param>
+        /// <param name="storageChangeSet">The storage change set.</param>
+        private static void CallBackNumberChange(string subscriptionId, StorageChangeSet storageChangeSet)
+        {
+            if (storageChangeSet.Changes == null 
+                || storageChangeSet.Changes.Length == 0 
+                || storageChangeSet.Changes[0].Length < 2)
+            {
+                // Logger.Warn("Couldn't update account informations. Please check 'CallBackAccountChange'");
+                return;
+            }
+
+            var numberInfoString = storageChangeSet.Changes[0][1];
+
+            if (string.IsNullOrEmpty(numberInfoString))
+            {
+                return;
+            }
+
+            Console.WriteLine("New Number Change: " + numberInfoString);
+        }
+
+        private static string GetClientConnectionStatus(SubstrateClient client)
+        {
+            return client.IsConnected ? "Connected" : "Not connected";
+
+        }
+    }
+}
---
+++ Ajuna.SDK.Demos.ServiceSubscription/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.WebSockets;
+using Ajuna.SDK.Demos.RestClient;
+using Ajuna.SDK.Demos.RestClient.Generated.Clients;
+using Ajuna.ServiceLayer.Model;
+
+namespace Ajuna.SDK.Demos.ServiceSubscription
+{
+    internal static class Program
+    {
+        // Websocket and API addresses of the Service layer - You need Ajuna.SDK.Demos.RestService running for this console app to run
+        private static String WebsocketUrl = "ws://localhost:61752/ws";
+        private static String ServiceUrl = "http://localhost:61752/";
+        
+        public static async Task Main(string[] args)
+        {
+            // Create BaseSubscriptionClient and connect
+            var subscriptionClient = new BaseSubscriptionClient(new ClientWebSocket());
+            await subscriptionClient.ConnectAsync(new Uri(WebsocketUrl), CancellationToken.None);
+             
+            // Assign Generic Handler for Storage Change
+            subscriptionClient.OnStorageChange = HandleChange;
+
+            // Create HttpClient
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(ServiceUrl)
+            };
+            
+            // Create SystemControllerClient
+            var systemControllerClient = new SystemControllerClient(httpClient, subscriptionClient);
+
+            // Subscribe to Number Changes
+            var subscribedSuccessfully = await systemControllerClient.SubscribeNumber();
+            
+            // Continue only if Subscription has succeeded
+            if (subscribedSuccessfully)
+            {
+                Console.WriteLine("Successfully Subscribed. Now listening for Storage Changes...");
+                Console.WriteLine("Press ESC to exit");
+            }
+            else
+            {
+                Console.WriteLine("Subscription failed. Exiting...");
+                return;
+            }
+
+            // Keep reading the stream waiting for Storage Changes and exit when the user presses the ESCAPE key
+            bool listenForStorageChanges = true;
+            while (listenForStorageChanges)
+            {
+                await subscriptionClient.ReceiveNextAsync(CancellationToken.None);
+
+                if (Console.KeyAvailable && Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    listenForStorageChanges = false;
+                }
+            }
+         
+            // Close Websocket Connection 
+            await subscriptionClient.CloseAsync(WebSocketCloseStatus.Empty,"",CancellationToken.None);
+        }
+
+        private static void HandleChange(StorageChangeMessage message)
+        {
+            Console.WriteLine("New Change: " + message.Data);
+        }
+    }
+}
